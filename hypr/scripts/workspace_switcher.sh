9d105cd6fd5ded5ae82d1624e850c9c0be1061f0
ysh <yash110704@gmail.com>
2025-05-05 22:59:02 +0530

    removing redundant dotfiles

--- hypr/scripts/workspace_switcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-active_workspaces=$(hyprctl workspaces -j | jq -r '.[].name')
-
-options=$(echo -e "$active_workspaces\nNew Workspace")
-
-selected=$(echo "$options" | rofi -dmenu -i -p "Workspaces:")
-
-hyprctl dispatch workspace "$selected"
