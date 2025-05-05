116de54f6eb874e90fb3a234c5ab18cec78ea708
ysh <yash110704@gmail.com>
2025-05-05 22:34:37 +0530

    selecting dotfiles

--- hypr/scripts/workspace_switcher.sh
+++ hypr/scripts/workspace_switcher.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Get the list of all active workspaces
 active_workspaces=$(hyprctl workspaces -j | jq -r '.[].name')
 
-# Generate options for rofi
 options=$(echo -e "$active_workspaces\nNew Workspace")
 
-# Use rofi for selection
 selected=$(echo "$options" | rofi -dmenu -i -p "Workspaces:")
 
 hyprctl dispatch workspace "$selected"
